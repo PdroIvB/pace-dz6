8eddda9e5f2c9e17fd690f9f4fdb26478a021bf1
PdroIvB <pedroicbarbos@gmail.com>
2024-11-05 18:54:36 -0300

    cria migrations iniciais e instala pacotes de drag n drop

---
+++ database/migrations/2024_11_05_214532_create_permissions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('can_read')->default(false);
+            $table->boolean('can_edit')->default(false);
+            $table->foreignId('workspace_id');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions');
+    }
+};
